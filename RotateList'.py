4e37fc8fc39cb0ea02af6a32f434d9bed18aef86
ImthiyazBasha13 <78927876+ImthiyazBasha13@users.noreply.github.com>
2021-02-25 08:35:36 +0530

    Add files via upload

---
+++ RotateList'.py
@@ -0,0 +1,9 @@
+# Python program to right rotate 
+# a list by n using list slicing 
+n = 3
+
+list_1 = [1, 2, 3, 4, 5, 6] 
+list_1 = (list_1[len(list_1) - n:len(list_1)] 
+				+ list_1[0:len(list_1) - n]) 
+print(list_1) 
+
